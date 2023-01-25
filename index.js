fe1469bc7f897463d638440c1ea6465f64026e14
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-01-25 09:25:00 +0200

    fetch 200

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log(1);
+fetch('./images/__in/data.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(handleData)
+  .catch(handleError);
+
+function handleData(data) {
+  console.log(data);
+  sortData(data);
+
+  //   const { id, age, image, rating, tags, title, url } = data;
+}
+
+function handleError(error) {
+  console.log(error.message);
+}
+
+function sortData(data) {
+  data.sort((firstElem, secondElem) => secondElem.rating - firstElem.rating);
+}
