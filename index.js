5cca29f4ada8fd5dcc9a7b11af03ede8da9915a3
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-01-25 11:18:43 +0200

    render images to 768px

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const refs = {
+  featuredContainer: document.querySelector('.js-featured-container'),
+  featuredList: document.querySelector('.js-featured-list'),
+};
+
 fetch('./images/__in/data.json')
   .then(response => {
     if (!response.ok) {
@@ -10,15 +15,48 @@ fetch('./images/__in/data.json')
 
 function handleData(data) {
   console.log(data);
-  sortData(data);
+  const markupFeaturedImages = createMarkupFeaturedListSm(data);
 
-  //   const { id, age, image, rating, tags, title, url } = data;
-}
-
-function handleError(error) {
-  console.log(error.message);
+  refs.featuredList.innerHTML = markupFeaturedImages;
 }
 
 function sortData(data) {
   data.sort((firstElem, secondElem) => secondElem.rating - firstElem.rating);
+}
+
+function createMarkupFeaturedListSm(data) {
+  sortData(data);
+  const mobileImagesArray = data.slice(0, 5);
+
+  return mobileImagesArray
+    .map(imageElem => {
+      const { id, age, image, rating, tags, title, url } = imageElem;
+      const tagsToShow = handleTags(tags);
+
+      return `<li class="featured__item">
+              <a href="${url}" class="featured__link">
+                <div class="img-wrapper">
+                  <img
+                    src="./images/__in/${image}"
+                    alt="${title}"
+                    class="featured__img"
+                    loading="lazy"
+                  />
+                </div>
+                <div class="img__info">
+                  <p class="img__name">${title}</p>
+                  <p class="img__tags">${tagsToShow}</p>
+                </div>
+              </a>
+            </li>`;
+    })
+    .join('');
+}
+
+function handleTags(tags) {
+  return tags.map(tag => `#${tag}`).join(', ');
+}
+
+function handleError(error) {
+  console.log(error.message);
 }
